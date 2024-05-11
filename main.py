191ca8772303313dbe5350bf70d21feaa240dcf9
Filipe Queiroz <filipesantiago.qrz@gmail.com>
2024-05-11 16:32:27 -0300

    feat: Commit inicial contendo chat operacional

---
+++ main.py
@@ -0,0 +1,78 @@
+import google.generativeai as genai
+import PIL.Image
+import pathlib
+import textwrap
+import sys
+import json
+
+from IPython.display import display
+from IPython.display import Markdown
+
+import os
+from dotenv import load_dotenv
+
+class Hemilia:
+    def __init__(self):
+        load_dotenv()
+        self.load_api_key()
+        MODEL_NAME_VISION="gemini-pro-vision"
+        MODEL_NAME="gemini-1.5-pro-latest"
+
+        self.model_vision = genai.GenerativeModel(model_name=MODEL_NAME_VISION)
+        self.model = genai.GenerativeModel(model_name=MODEL_NAME)
+
+        # history = self.process_images()
+        # self.save_json("model_base.json", history)
+        history = self.load_history_from_file("model_base.json")
+        self.start_chat(history=history)
+        
+    def start_chat(self, history):
+        self.add_to_history("Se apresente como Hemília, uma assistente pessoal para informações sobre Hemofilia e Responda de acordo com as informações fornecidas", history=history)
+        chat = self.model.start_chat(history=history)
+        prompt = input('Esperando prompt: ')
+
+        while prompt != "fim":
+            response = chat.send_message(prompt)
+            print("Resposta:", response.text, '\n\n')
+            prompt = input('Esperando prompt: ')
+
+
+    def load_history_from_file(self, file_name):
+        with open(file_name) as f:
+            data = json.load(f)
+            return data
+
+    def save_json(self, file_name, data):
+        with open(file_name, 'w') as f:
+            json.dump(data, f)
+
+    def process_images(self):
+        folder = pathlib.Path("./images/")
+        history = []
+        for item in folder.glob("*.jpeg"):
+            img = PIL.Image.open(item)
+            response = self.model_vision.generate_content(img)
+            self.add_to_history(response.text, history=history)
+        return history
+        
+    def load_api_key(self):
+        genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+    def to_markdown(self, text):
+        text = text.replace('•', '  *')
+        return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+    def add_to_history(self, text, history):
+        action = {}
+        action["role"] = "user"
+        action["parts"] = [text]
+        history.append(action)
+
+def main():
+    try:
+        start = Hemilia()
+    except ValueError as ve:
+        return str(ve)
+
+if __name__ == "__main__":
+    sys.exit(main())
